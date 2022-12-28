75619ef2a38d708fc22254edc4e1047ab2d8e4a0
alecubillos <alecubillos@gmail.com>
2022-12-28 15:05:17 -0800

    add remaining tests

--- cypress/e2e/greenkart.cy.js
+++ cypress/e2e/greenkart.cy.js
@@ -6,16 +6,26 @@ const { greenkart } = require("../POM/greenkart");
         cy.visit('');
       })
                      
-        it('Add to cart', () => {
+        it('Add to cart two products', () => {
           greenkart.getTotalCartItems().invoke('text').then((initialTotal)=>{
             greenkart.addProductToCart('Brocolli');
+            greenkart.addProductToCart('Beetroot')
             greenkart.getTotalCartItems().invoke('text').then((finalTotal)=>{
-                expect(parseInt(finalTotal)).eql(parseInt(initialTotal)+1);
+                expect(parseInt(finalTotal)).eql(parseInt(initialTotal)+2);
             });
           })    
         })
+
+        it('Remove until one item on cart', ()=> {
+          greenkart.addProductToCart('Brocolli');
+          greenkart.addProductToCart('Beetroot');
+          greenkart.removeProductFromCartByName('Brocolli');
+          greenkart.getTotalCartItems().invoke('text').then((finalTotal)=>{
+            expect(parseInt(finalTotal)).eql(1);
+        });
+      })
         
-        it('Remove from cart', ()=> {
+        it('Remove from cart until empty', ()=> {
             greenkart.addProductToCart('Brocolli');
             greenkart.removeProductFromCartByName('Brocolli');
             greenkart.elements.getEmptyCartMsg().should('have.text', 'You cart is empty!');
@@ -24,5 +34,10 @@ const { greenkart } = require("../POM/greenkart");
         it('Fail to remove product from empty cart', ()=>{
             greenkart.clickPreviewCart();
             greenkart.elements.getRemoveProductBtn().should('not.exist');
+        })
+
+        it('Fail test when attempting to remove product with empty cart', ()=>{          
+            greenkart.clickPreviewCart();
+            greenkart.removeProductFromCartByName('Brocollii');
         })
     })
